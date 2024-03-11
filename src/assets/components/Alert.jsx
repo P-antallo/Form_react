5a1f7c5a11b2629d3dbd744031ac25a910f7ac64
Juanca Bustamante <bustamantebravo.jc@gmail.com>
2024-03-10 22:37:00 -0300

    Añade efecto de transición al componente Alert

--- src/assets/components/Alert.jsx
+++ src/assets/components/Alert.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 const Alert = ({ message, type }) => {
   return (
-    <div className={`alert alert-${type}`} role="alert">
-      {message}
+    <div className="alert-transition">
+      {message && (
+        <div className={`alert alert-${type}`} role="alert">
+          {message}
+        </div>
+      )}
     </div>
   );
 };
 
 export default Alert;
+
